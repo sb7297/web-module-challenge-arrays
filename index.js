c8b3b5acce484ceac419dfddfbc03abc47c43eb9
sb7297 <sbaireddy7297@gmail.com>
2021-06-09 10:18:14 -0400

    index.js: task 1 completed

--- index.js
+++ index.js
@@ -45,13 +45,9 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(input_array){
+    return input_array.map(x => x);
 }    
-
-
-
-
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Confirm that an array is exactly 31 flavors. Your function should accept:
